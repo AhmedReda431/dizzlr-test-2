baf50b82e184ece519a52664125db1bbc97e2867
Ahmed Reda <ahmedreda431@gmail.com>
2020-10-31 16:19:31 +0200

    this is second cordova test for dizzlr company

---
+++ js/custom.js
@@ -0,0 +1,91 @@
+{
+  /* <div class="myCarousel">
+<div
+  id="carouselExampleIndicators"
+  class="carousel slide"
+  data-ride="carousel"
+>
+  <ol class="carousel-indicators">
+    <li
+      data-target="#carouselExampleIndicators"
+      data-slide-to="0"
+    ></li>
+    <li
+      data-target="#carouselExampleIndicators"
+      data-slide-to="1"
+    ></li>
+    <li
+      data-target="#carouselExampleIndicators"
+      data-slide-to="2"
+      class="active"
+    ></li>
+    <li
+      data-target="#carouselExampleIndicators"
+      data-slide-to="3"
+    ></li>
+    <li
+      data-target="#carouselExampleIndicators"
+      data-slide-to="4"
+    ></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active my-item">
+      <img
+        src="style/assets/3.webp"
+        class="d-block w-100"
+        alt="..."
+      />
+    </div>
+    <!-- <div class="carousel-item my-item">
+      <img
+        src="style/assets/3.webp"
+        class="d-block w-100"
+        alt="..."
+      />
+    </div>
+    <div class="carousel-item active">
+      <img
+        src="style/assets/carousel1.PNG"
+        class="d-block w-100"
+        alt="..."
+      />
+    </div>
+    <div class="carousel-item my-item">
+      <img src="style/assets/2.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item my-item">
+      <img
+        src="style/assets/3.webp"
+        class="d-block w-100"
+        alt="..."
+      />
+    </div> -->
+  </div>
+  <a
+    class="carousel-control-prev"
+    href="#carouselExampleIndicators"
+    role="button"
+    data-slide="prev"
+  >
+    <span
+      class="carousel-control-prev-icon"
+      aria-hidden="true"
+    ></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a
+    class="carousel-control-next"
+    href="#carouselExampleIndicators"
+    role="button"
+    data-slide="next"
+  >
+    <span
+      class="carousel-control-next-icon"
+      aria-hidden="true"
+    ></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div> */
+}
+
